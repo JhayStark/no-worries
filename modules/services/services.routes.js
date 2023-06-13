a15d3801cc536ac1582344539bfbc1adcc175a1b
JhayStark <jaamen5@gmail.com>
2023-06-13 15:32:19 -0700

    added route to add service

--- modules/services/services.routes.js
+++ modules/services/services.routes.js
@@ -2,6 +2,7 @@ const {
   getAllServices,
   getServiceByTag,
   searchService,
+  addService,
 } = require("./services.controller");
 const router = require("express").Router;
 
@@ -10,5 +11,6 @@ const serviceRouter = router();
 serviceRouter.get("/all-services", getAllServices);
 serviceRouter.get("/service-by-tag", getServiceByTag);
 serviceRouter.get("/search-service", searchService);
+serviceRouter.post("/add-service", addService);
 
 module.exports = serviceRouter;
